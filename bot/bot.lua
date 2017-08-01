f560db27693782954cbf215e5d54074f144689b0
oralius <rainy.always@gmail.com>
2017-08-01 20:10:48 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -97,7 +97,7 @@ function create_config( )
     info_text = [[》bot v6.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/oralius/antiv6
+》https://github.com/oralius
 
 》Admins :
 》@oralius ➣ Founder & Developer《
