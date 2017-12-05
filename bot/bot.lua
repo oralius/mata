367e41911f82dd01169f4d5696775812a1740e07
oralius <rainy.always@gmail.com>
2017-12-06 00:07:33 +0330

    Add files via upload

--- bot/bot.lua
+++ bot/bot.lua
@@ -19,7 +19,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
-helper_username = '434704112'  -- Input Helper Username Here Without @
+helper_username = 'Helper_Telecontrol_bot'  -- Input Helper Username Here Without @
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
@@ -97,20 +97,20 @@ function create_config( )
     info_text = [[》bot v6.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/oralius/anti.*ربات ضد لينك و ضد تبليغات در گروهها با قابليت قفل كامل چت و تمام موارد ارسالي.*
+》https://github.com/oralius/anti
+
 》Admins :
-》@oralius ➣ FounderDeveloper
-》@oralius ➣ Developer
+》@oralius ➣ Founder & Developer《
+》@oralius ➣ Developer《
+
+》Special thanks to
 》
-》*لينك ورود براي خريد ربات*
-》https://t.me/oralius
+
 》Our channel :
 》@activebots
-》*براي خريد اين ربات به ايدي زير پيام دهيد* :
-》
-》@oralius 
-》
-》
+
+》Our website :
+》http://lua.org
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
